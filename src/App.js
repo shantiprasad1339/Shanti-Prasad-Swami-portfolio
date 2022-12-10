6b3f75684e115b40e14a40e8769eb7aa816bf169
dutchman <shantiprasad1339@gmail.com>
2022-12-10 10:35:48 +0530

    more touching

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import Boots from './Boots';
 import Mainpage from './Portfolio/Mainpage';
 import './Portfolio/Portfoliocss.css'
+import {BrowserRouter} from 'react-router-dom';
 import Technicalskills from './Portfolio/Technicalskills';
 import Rcentprojects from './Portfolio/Rcentprojects';
 import Aboutpage from './Portfolio/Aboutpage';
 import Contacts from './Portfolio/Contacts';
+import Navbar from './Portfolio/Navbar';
 function App() {
   return (
     <>
-    
+    <BrowserRouter>
+    <Navbar />
     {/* // <Boots /> */}
     <Mainpage />
     <Technicalskills />
     <Rcentprojects />
     <Aboutpage />
     <Contacts />
-
+</BrowserRouter>
     </>
   );
 }
--- src/Portfolio/Mainpage.jsx
+++ src/Portfolio/Mainpage.jsx
@@ -1,27 +1,47 @@
 import React, { component } from 'react'
 
-
 const Mainpage = () => {
   return (
     <>
-      <section id="header">
-        
-        <nav>
-          
-          <ul className='ul'>
-            <li><a href="<Aboutpage.jsx/>">Home</a></li>
-            <li><a href="">about</a></li>
-            <li><a href="">contacts</a></li>
-            <li><a href="">footer</a></li>
+    
+    <section id="header"> 
+    <nav class="navbar navbar-expand-lg .bg-transparent fs-3">
+  <div class="container-fluid ">
+    <a class="navbar-brand active  text-light rounded-2 border border-1 px-2" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-light rounded-2  px-2" aria-current="page" href="#">Aboutpage</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light  rounded-2  px-2" href="#">Contacts</a>
+        </li>
+       
+        <li class="nav-item dropdown text-light">
+          <a class="nav-link dropdown-toggle text-light  rounded-2  px-2" href="#" role="button text-light" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        </nav>
-        <h1 className="brand" ><span></span></h1>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
         <h1 className='neon ' data-text="SHANTIPRASAD">SHANTI PRASAD</h1>
       </section>
 
 
       <section id="hero" >
+      
         <div className="hero contaniner"  >
+        
           <div>
             <h1>Hello ,<span></span></h1>
             <h1>My Name is<span></span> </h1>
